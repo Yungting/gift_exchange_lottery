9ea3fac237c4e50733868d97d9e0e3bda271157e
Tina <tina.liu@howsense.shop>
2023-11-30 23:57:47 +0800

    新增姓名陣列

---
+++ lottery.js
@@ -0,0 +1,90 @@
+const peopleList = [
+  { id: 1, name: 'Person 1', gift_to: 10 },
+  { id: 2, name: 'Person 2', gift_to: 8 },
+  { id: 3, name: 'Person 3', gift_to: 18 },
+  { id: 4, name: 'Person 4', gift_to: 9 },
+  { id: 5, name: 'Person 5', gift_to: 11 },
+  { id: 6, name: 'Person 6', gift_to: 12 },
+  { id: 7, name: 'Person 7', gift_to: 16 },
+  { id: 8, name: 'Person 8', gift_to: 7 },
+  { id: 9, name: 'Person 9', gift_to: 3 },
+  { id: 10, name: 'Person 10', gift_to: 17 },
+  { id: 11, name: 'Person 11', gift_to: 2 },
+  { id: 12, name: 'Person 12', gift_to: 4 },
+  { id: 13, name: 'Person 13', gift_to: 19 },
+  { id: 14, name: 'Person 14', gift_to: 1 },
+  { id: 15, name: 'Person 15', gift_to: 13 },
+  { id: 16, name: 'Person 16', gift_to: 6 },
+  { id: 17, name: 'Person 17', gift_to: 15 },
+  { id: 18, name: 'Person 18', gift_to: 14 },
+  { id: 19, name: 'Person 19', gift_to: 5 },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const nameList = [
+  "國哥",
+  "衣布",
+  "嘎嘎",
+  "嬸嬸",
+  "泰成哥",
+  "慧琦嫂",
+  "大哥",
+  "語宸嫂",
+  "大姊",
+  "小齊哥",
+  "小姊",
+  "小哥",
+  "小魚",
+  "計哥",
+  "嘉榕",
+  "阿成",
+  "宜穎",
+  "劉小妹",
+  "小胖"
+];
+const randomNameList = [
+  "嘉榕",
+  "計哥",
+  "小齊哥",
+  "小魚",
+  "大哥",
+  "嬸嬸",
+  "小哥",
+  "泰成哥",
+  "小姊",
+  "衣布",
+  "國哥",
+  "宜穎",
+  "大姊",
+  "慧琦嫂",
+  "小胖",
+  "嘎嘎",
+  "阿成",
+  "語宸嫂",
+  "劉小妹"
+]
+
+
